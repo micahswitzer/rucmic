01c74a33ec70b608dd5b624eaae8e2d4369cce1e
Micah Switzer <micah.switzer.dev@outlook.com>
2020-05-04 23:47:38 -0400

    Updated program to have flexible inputs and outputs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![allow(unused_braces)]
 
-use std::io::Read;
+use std::io::{ Read, Write };
+use std::fs::File;
+use std::error::Error;
 use atty::Stream;
 use clap::Clap;
 
@@ -9,39 +11,55 @@ mod lexer;
 mod parser;
 //mod codegen;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let opts: Opts = Opts::parse();
 
-    match opts.infile {
-        Some(fname) => { eprintln!("you provided an input file named '{}'.", fname); },
+    let mut infile: Box<dyn Read> = match opts.infile {
+        Some(fname) => {
+            eprintln!("you provided an input file named '{}'.", fname);
+            Box::new(File::open(fname)?)
+        },
         None => { 
             eprintln!("you didn't provide an input file.");
             if atty::is(Stream::Stdin) {
-                eprintln!("additionally, stdin is a tty.");
+                eprintln!("additionally, stdin is a tty. this is probably not what you wanted.");
             }
+            Box::new(std::io::stdin())
         },
-    }
-     match opts.outfile {
-        Some(fname) => { eprintln!("you provided an output file named '{}'.", fname); },
+    };
+    let _outfile: Box<dyn Write> = match opts.outfile {
+        Some(fname) => {
+            eprintln!("you provided an output file named '{}'.", fname);
+            Box::new(File::create(fname)?)
+        },
         None => {
             eprintln!("you didn't provide an output file.");
             if atty::is(Stream::Stdout) {
                 eprintln!("additionally, stdout is a tty. no code will be output.");
+                // this is a dummy sink for the output data
+                //Box::new(std::io::sink())
             }
+            else { }
+            // always output to stdout for testing purposes
+            Box::new(std::io::stdout())
         },
-    }
+    };
 
     let mut s = String::new();
-    std::io::stdin().read_to_string(&mut s).unwrap();
+    infile.read_to_string(&mut s)?;
     // lex the input, and print the tokens as it munches them
-    let lexer = lexer::Lexer::new(&s).inspect(|tok| eprintln!("tok: {:?}", tok));
+    let lexer = lexer::Lexer::new(&s).inspect(|tok| if cfg!(debug_assertions) { eprintln!("tok: {:?}", tok) });
     // parse the tokens
-    let program = parser::parse(lexer).unwrap();
+    let program = parser::parse(lexer)?;
     // print the ast
+    #[cfg(debug_assertions)]
     eprintln!("prog: {:?}", program);
     // generate the code!
     //let codegen = codegen::CodeGen::new("main");
     //let code = codegen.gen(&program).unwrap();
+
+    // ok...we made it
+    Ok(())
 }
 
 #[derive(Clap)]
